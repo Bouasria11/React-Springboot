6de33f1045b00c262767c0f93de852f7ad47d2b8
Bouasria11 <130832631+Bouasria11@users.noreply.github.com>
2026-05-14 16:23:47 +0100

    Delete backend directory
    second commit

--- backend/src/main/java/com/cinestack/api/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cinestack.api.repository;
-
-import com.cinestack.api.domain.Genre;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-    Optional<Genre> findByNameIgnoreCase(String name);
-}
--- backend/src/main/java/com/cinestack/api/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cinestack.api.repository;
-
-import com.cinestack.api.domain.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
-}
